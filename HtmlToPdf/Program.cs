8c6bed949114cac264dcda9bbc6878fd4eea0a7b
Amin Bennayev <129307929+Am1nn@users.noreply.github.com>
2024-10-11 01:48:44 +0400

    Add project files.

---
+++ HtmlToPdf/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aspose.Pdf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crate Option
+        HtmlLoadOptions htmloptions = new HtmlLoadOptions();
+        // Find And Load Html Document 
+        Document doc = new Document(@"C:\Users\Amin\Desktop\test.html", htmloptions);
+        // Save Converted pdf document
+        doc.Save("example.pdf");
+
+        //Use Aspose NuGet Package 
+        //Version (24.9.0)
+    }
+
+    
+}
